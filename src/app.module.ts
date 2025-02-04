56b62c11bd6f14214cf0431c7df379dac999f428
Denys Sttrilchuk <denia876@gmail.com>
2025-02-04 20:13:14 +0200

    Added User model to MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+import {Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
+import {MongooseModule} from '@nestjs/mongoose';
 import * as process from "process";
+import {UsersModule} from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://admin:password@localhost:27017'),
+    UsersModule,
   ],
 })
 export class AppModule {}
