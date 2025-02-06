96ee9312fa00cd137d3aeee5bf048a29fe876b23
Denys Sttrilchuk <denia876@gmail.com>
2025-02-06 17:15:22 +0200

    feat(users): added email uniqueness check when updating

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import {Injectable, NotFoundException} from '@nestjs/common';
+import {ConflictException, Injectable, NotFoundException} from '@nestjs/common';
 import {InjectModel} from '@nestjs/mongoose';
 import {Model} from 'mongoose';
 import {User, UserDocument} from './schema/user.schema';
@@ -57,16 +57,22 @@ export class UsersService {
   }
 
   async updateUser(userId: string, updateData: UpdateUserDto) {
-    const updatedUser = await this.userModel.findByIdAndUpdate(userId, updateData, {
-      new: true,
-      runValidators: true,
-    });
-    if (!updatedUser) {
-      throw new NotFoundException('User not found');
+    try {
+      const updatedUser = await this.userModel.findByIdAndUpdate(userId, updateData, {
+        new: true,
+        runValidators: true,
+      });
+      if (!updatedUser) {
+        new NotFoundException('User not found');
+      }
+      return updatedUser;
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new ConflictException('Email already exists');
+      }
+      throw error;
     }
-    return updatedUser;
   }
-
 
   async deleteUser(userId: string) {
     const user = await this.userModel.findByIdAndDelete(userId);
